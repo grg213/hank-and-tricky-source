f325e673bf595b41382581673d99f267f697a3e3
grg213 <92542785+grg213@users.noreply.github.com>
2021-10-14 15:44:56 -0700

    tiky funny
    sfdasffasfasdfasfdsf

---
+++ source/joke.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.animation.FlxBaseAnimation;
+import flixel.graphics.frames.FlxAtlasFrames;
+
+using StringTools;
+
+class joke extends FlxSprite
+{
+     var test:String = "yes";
+     deez = test;
+     nuts = deez;
+     add(deez + nuts);
+}
